bc6d13b9cde283d11bc76c6a5d20f242ddec6e0f
Rajkumar06706 <122003836+Rajkumar06706@users.noreply.github.com>
2026-07-07 15:47:34 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,22 +1,19 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = ">= 6.0.0"
     }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
-    }
+
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = ">= 6.0.0"
     }
   }
 
-  # Uncomment to use remote state
+  # Uncomment this block only when you want to use GCS remote backend
   # backend "gcs" {
   #   bucket = "your-terraform-state-bucket"
   #   prefix = "terraform/state"
@@ -24,11 +21,6 @@ terraform {
 }
 
 provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
-
-provider "google-beta" {
   project = var.gcp_project_id
   region  = var.gcp_region
 }
